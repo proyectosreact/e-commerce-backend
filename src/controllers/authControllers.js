1eaeceb807aefda6760524cec099314bc82cb46a
Fernando Andres Lopez <fernando.lopez1@ibm.com>
2020-05-29 16:00:55 -0300

    Update

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,57 @@
+const User = require('../models/User');
+const bcryptjs = require ('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+
+exports.authUser = async (req, res) =>{
+    //check for mistakes
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ){
+        res.status(400).json({ errors: errors.array() })
+    }
+
+    //Extract email and password
+    const { email, password } = req.body
+
+    try {
+        //check that the user is registered
+        let user = await User.findOne({ email });
+        if(!user){
+            return res.status(400).json({ msg: 'The username does not exist' });
+        }
+
+        //check password
+        const correctPass = await bcryptjs.compare( password, user.password );
+        if(!correctPass){
+            return res.status(400).json({ msg: 'Wrong Password' })
+        }
+
+        //if everything is correct Create and sing the JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+
+        };
+
+        //Sing JWT
+        jwt.sign(payload, process.env.SECRET, {
+
+            expiresIn: 14400
+        }, 
+        (error, token) => {
+            if(error) throw error;
+
+            //Confirmation Msg
+            res.json({ token });
+
+        });
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
---
+++ src/routes/user.js
@@ -0,0 +1,18 @@
+//Route to make users
+const express = require('express');
+const router = express.Router();
+const userController = require( '../controllers/userController' );
+const { check } = require('express-validator');
+
+
+//Make a user
+//api/user
+router.post( '/', [
+    check( 'name', 'The name is required').not().isEmpty(),
+    check('email', 'Add a valid email').isEmail(),
+    check('password', 'The password must be a minimum of 8 characters').isLength({ min: 8 })
+],
+ userController.createUser )
+
+
+module.exports = router;
