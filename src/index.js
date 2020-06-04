c8ec6c85bcb6de575a9020a9501709524b769ecd
fernandoandres87 <fer_87_5@hotmail.com>
2020-06-04 14:11:07 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const connectedDB = require('./config/dataBase');
 
 //make server
 const app = express();
+app.use(cors());
 
 //conect DB
 connectedDB();
@@ -11,12 +12,14 @@ connectedDB();
 //Express.json
 app.use(express.json({ extended: true }));
 
+
 //PORT app
 const PORT = process.env.PORT || 4000;
 
 //Import route user
 app.use('/api/users' , require( './routes/user' ));
 app.use('/api/auth' , require( './routes/auth' ));
+
 
 //start app
 app.listen( PORT, () =>{
