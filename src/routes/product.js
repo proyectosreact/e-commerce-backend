c87e1e4ce83a010d44ca16fb6228bb1376c5cb3a
Enzo <ebarlotta@yahoo.com.ar>
2020-06-10 19:32:43 -0300

    Add CtrlProduct - Create/Query/QueryById

--- src/routes/product.js
+++ src/routes/product.js
@@ -6,3 +6,11 @@ const { check } = require('express-validator');
 // Usar las validaciones para la SubCategory y Product
 
 router.post('/', productController.createProduct);
+
+router.get('/',productController.queryProduct)
+router.get('/:IdCategory',productController.queryProductId)
+
+router.put('/:IdCategory',productController.updateProductId)
+router.delete('/:IdCategory',productController.deleteProductId)
+
+module.exports=router;
