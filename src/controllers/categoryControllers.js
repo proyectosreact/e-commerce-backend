983f95eb1c3b08b5ef552da0cc40d0adfa03f790
otto-ajanel <46831467+otto-ajanel@users.noreply.github.com>
2020-06-22 02:50:00 +0200

    subcategory

--- src/controllers/categoryControllers.js
+++ src/controllers/categoryControllers.js
@@ -1,83 +1,121 @@
-const Category=require('../models/category');
-const {validationResult}=require('express-validator');
+const Category = require('../models/category');
+const {
+  validationResult
+} = require('express-validator');
 
 
-exports.createCategory = async(req, res) => {
+exports.createCategory = async (req, res) => {
 
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+
+  const {
+    category
+  } = req.body;
+
+
+  try {
+    let categoryName = await Category.findOne({
+      category
+    });
+
+    if (categoryName) {
+      return res.status(400).json({
+        msg: 'Category already exists'
+      });
     }
- 
-    const {category} = req.body;
-    console.log({ category });
-    
-    try {
-        let categoryName = await Category.findOne({ category });
-         console.log("no ha pasasdo en el try")
-        if(categoryName){
-            return res.status(400).json({msg: 'Category already exists'});
-        }
 
-        categoryName = new Category(req.body);
-        await categoryName.save()
+    categoryName = new Category(req.body);
+    await categoryName.save()
 
-        res.json({status: true, msg: 'The category was inserted correctly'});
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({msg: 'There was a mistake'});
-    }   
+    res.json({
+      status: true,
+      msg: 'The category was inserted correctly'
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: 'There was a mistake'
+    });
+  }
 }
 
-exports.queryCategory = async(req, res) => {
-    Category.find({}, (err, categorys) => {
-        if (err) {
+exports.queryCategory = async (req, res) => {
+  Category.find({}, (err, categorys) => {
+    if (err) {
 
-            return res.status(500).json({ message: `Erro this petition ${err}` })
-        }
-        if (!categorys) {
-            return res.status(404).json({ message: `The categorys not exist ` })
+      return res.status(500).json({
+        message: `Erro this petition ${err}`
+      })
+    }
+    if (!categorys) {
+      return res.status(404).json({
+        message: `The categorys not exist `
+      })
 
-        }
-        res.status(200).send({categorys})
+    }
+    res.status(200).json({
+      categorys
     })
+  })
 }
-exports.queryCategoryId = async(req, res) => {
-    let categoryId = req.params.IdCategory
-    Category.findById(categoryId, (err, category) => {
-        if (err) {
+exports.queryCategoryId = async (req, res) => {
+  let categoryId = req.params.IdCategory
+  Category.findById(categoryId, (err, category) => {
+    if (err) {
 
-            return res.status(500).json({ message: `Erro this petition ${err}` })
-        }
-        if (!category) {
-            return res.status(404).json({ message: `The category not exist ` })
+      return res.status(500).json({
+        message: `Erro this petition ${err}`
+      })
+    }
+    if (!category) {
+      return res.status(404).json({
+        message: `The category not exist `
+      })
 
-        }
-        res.send(200, { category })
+    }
+    res.status(200).json({
+      category
     })
+  })
 }
-exports.updateCategoryId = async(req, res) => {
-    let categoryId = req.params.IdCategory
-    let update = req.body
+exports.updateCategoryId = async (req, res) => {
+  let categoryId = req.params.IdCategory
+  let update = req.body
 
-    Category.findByIdAndUpdate(categoryId, update, (err, category) => {
-        if (err) {
+  Category.findByIdAndUpdate(categoryId, update, (err, category) => {
+    if (err) {
 
-            return res.status(500).json({ message: `Erro on update category ${err}` })
-        }
+      return res.status(500).json({
+        message: `Erro on update category ${err}`
+      })
+    }
 
-        res.status(200).json({ category })
+
+    res.status(200).json({
+      category
     })
+  })
 }
-exports.deleteCategoryId=async(req,res)=>{
-    let categoryId=req.params.IdCategory
-     Category.findById(categoryId,(err,category)=>{
-         if(err) res.status(500).json({message:`Error on delete Category${err}`})
+exports.deleteCategoryId = async (req, res) => {
+  let categoryId = req.params.IdCategory
+  Category.findById(categoryId, (err, category) => {
+    if (err) res.status(500).json({
+      message: `Error on delete Category${err}`
+    })
 
-         category.remove(err=>{
-             if(err) res.status(500).send({message: 'Erro this delete category'})
-             res.status(200).json({message:'This product has delete'})
-         })
-     })
+    category.remove(err => {
+      if (err) res.status(500).send({
+        message: 'Erro this delete category'
+      })
+      res.status(200).json({
+        message: 'This product has delete'
+      })
+    })
+  })
 }
--- src/controllers/subCategoryControllers.js
+++ src/controllers/subCategoryControllers.js
@@ -1,15 +1,92 @@
-const Category = require('../models/category');
-const {validationResult} = require('express-validator');
+const Category = require("../models/category");
+const {
+  validationResult
+} = require("express-validator");
+const category = require("../models/category");
 
-exports.cretaSubcategory = async ( req, res ) =>{
+exports.createSubCategory = async (req, res) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
 
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array() })
-    };
-
-    console.log(req.body)
-    const {subCategory, name} = req.body;
-
+  const subCategory = req.body;
+  const IdCategory = req.params.IdCategory;
+  let existsubcategory = await Category.findOne({
+    subCategory: subCategory
+  })
+  if (existsubcategory) {
+    return res.status(400).json({
+      message: 'This subcategory exist'
+    })
+  }
+  Category.findByIdAndUpdate(
+    IdCategory, {
+      $push: {
+        subCategory: subCategory,
+      },
+    }, {
+      strict: false,
+    },
+    (err, managerparent) => {
+      if (err) {
+        return res.status(500).json({
+          message: "this error",
+        });
+      }
+      return res.status(200).json({
+        subCategory,
+      });
+    }
+  );
+}
+/*
+exports.querySubCategoryByIdCategory = async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+  const IdCategory = req.params.IdCategory;
+  Category.findById(IdCategory, (err, category) => {
+    if (err) {
+      return res.status(500).json({
+        message: `Error this petition ${err}`
+      })
+    }
+    if (!category) {
+      return res.status(404).json({
+        mesage: 'This cateogry not Exist'
+      })
+    }
+    let {
+      subcateogys
+    } = category.subCategory
+    console.log(subcateogys)
+    res.status(200).json({
+      subcategory: category.subCategory
+    })
+  })
+}*/
+exports.querySubCategoryByIdCategory = async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+  const IdCategory = req.params.IdCategory;
+  Category.findOne({
+    _id: IdCategory
+  }).select('subCategory').populate('name').exec(function (err, category) {
+    if (err) {
+      res.status(5000).json({
+        message: 'error'
+      })
+    }
+  })
 }
