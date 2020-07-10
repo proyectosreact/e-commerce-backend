9c481be83a0d2c8a3fa56415eb014bb6e8e08f55
Fernando Andres Lopez <fernando.lopez1@ibm.com>
2020-07-10 16:01:40 -0300

    actualizacion ramas

--- src/models/category.js
+++ src/models/category.js
@@ -2,22 +2,20 @@ const {Schema, model}= require('mongoose'),
 
 
 CategorySchema = new Schema({
-
     category:{
-        type: String,
-        required: true
+        type: String
     },
     subCategorys:[{
         subCategory:{
             type: String,
+            
         },
         products:[{
             product:{
                 type: String,
             },
             sku:{
-                type: String,
-                unique: true
+                type: String
             },
             uri:{
                 type: String
@@ -28,11 +26,9 @@ CategorySchema = new Schema({
             stock:{
                 type: String
             },
-            //slug: ver si agregar    
-        }]
+             }]
     }]
-},{
-    timestamps:true}
+},{ timestamps:true }
 )
 module.exports = model('Category',CategorySchema);
 
