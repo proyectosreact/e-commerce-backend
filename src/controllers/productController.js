ad800f400ad202d6e3dc5ea1016332cc76cca899
jerson100 <juamkoo@gmail.com>
2020-07-11 16:12:12 -0500

    Add createProduct and listProductsByCategoriesAndSubsCategories

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const { validationResult } = require('express-validator');
 const status = require('../config/config');
+const Category = require('../models/category');
 
 exports.createProduct = async ( req, res ) => {
   const errors = validationResult(req);
@@ -8,22 +9,28 @@ exports.createProduct = async ( req, res ) => {
     return res.status(400).json({ code: status.ERROR, message: errors.array() });
   }
 
-  const { product } = req.body;
-
   try {
-    let newProduct = await Product.findOne({product});
 
-    if (newProduct) {
-      return res.status(400).json({ code: status.ERROR, message: 'Product already exists.' });
+    const {idCategory, idSubCategory, product} = req.body;
+   
+    const category = await Category.findOne({'_id':idCategory,'subCategorys._id':idSubCategory},{'subCategorys.$':1,_id:0});
+
+    if(category){
+
+      await Category.findOneAndUpdate(
+        {'_id':idCategory,'subCategorys._id':idSubCategory},
+        {$push:{'subCategorys.$.products':product}}
+      )  
+
+      return res.status(200).json({code: status.ERROR, msg:"Producto creado satisfactoriamente"});
+
     }
 
-    newProduct = new Product(req.body);
-    await newProduct.save();
-
-    return res.json({ code: status.OK, message: 'The product was inserted correctly' });
+    return res.status(400).json({msg:"No se pudo crear el producto"});
     
   } catch (error) {
-    return res.status(400).json({ code: status.ERROR, message: 'There was a mistake' });
+    console.log(error);
+    return res.status(400).json({ code: status.ERROR, msg: 'There was a mistake' });
   }
 
 }
@@ -36,25 +43,87 @@ exports.updateProduct = async ( req, res ) => {
   
 }
 
+
+//En pruebas
 exports.deleteProduct = async ( req, res ) => {
   
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  try{
+
+    //findByIdAndDelete
+    /*const product = await Category.findOneAndDelete (
+      {'subCategorys.products._id':req.param.id},
+      {'subCategorys.products.$._id':req.param.id}
+    );
+      */
+
+      console.log(req.params.id);
+    
+      const product = await Category.find({'subCategorys.products':{$elemMatch:{_id:req.params.id}}},
+                                          {'subCategorys.products.$':1,_id:0});
+
+    res.json(product);
+    //console.log(subCategoria);
+
+    
+
+/*
+    if(productDelete){
+      return res.status(200).json({msg:`Product ${productDelete.product} is deleted`});
+    }
+
+    console.log("No se pudo eliminar");*/
+
+  }catch(error){
+    console.log(error);
+    return res.status(400).json({msg:'The requested operation could not be performed'});
+  }
+
 }
 
-exports.listProducts = async ( req, res ) => {
-  // check for mistakes.
+
+exports.listProductsByCategoriesAndSubsCategories = async( req, res) => {
+
   const errors = validationResult(req);
 
   if ( !errors.isEmpty() ) {
     return res.status(400).json({ code: status.ERROR, message: errors.array() });
   }
 
+  //console.log(req.query);
+  const {idCategories,idSubCategory} = req.query;
+
   try {
-    const products = await Product.find();
-    const total = await Product.countDocuments();
 
-    return res.json({ code: status.OK, products, total });
+    const products = await Category.findOne(
+      {_id:idCategories,'subCategorys._id':idSubCategory},
+      {_id:0,'subCategorys.$':1}
+    );
 
+    //const total = await Category.countDocuments();
+
+    let productsItems = [],
+        total = 0;
+
+    if(products && products.subCategorys[0] && products.subCategorys[0].products){
+      
+      productsItems = products.subCategorys[0].products;
+  
+      total = productsItems.length;
+
+    }
+    
+    return res.json({ code: status.OK, products: productsItems, total });
+    
   } catch (error) {
+    
     return res.json({ code: status.ERROR, message: 'Internal server error' });
+
   }
-}
+
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -5,10 +5,20 @@ const { check } = require('express-validator');
 
 // Usar las validaciones para la SubCategory y Product
 
-router.post('/',[check('name', 'The name is required')], productController.createProduct);
-router.get('/list',productController.listProducts)
+//router.post('/',[check('name', 'The name is required')], productController.createProduct);
+router.post('/',[
+    check('idCategory', 'The idCategory is required').not().isEmpty(),
+    check('idSubCategory', 'The idSubCategory is required').not().isEmpty(),
+    check('product', 'The product is required').not().isEmpty(),
+    check('product.price', 'The price is required').not().isEmpty()
+],
+productController.createProduct);
 
-//router.put('/:IdCategory',productController.updateProductId)
+//router.get('/list',productController.listProducts);
+router.get('/list',productController.listProductsByCategoriesAndSubsCategories);
+router.delete('/:id',productController.deleteProduct);
+
+//router.put('/:IdCategory',productController.updateProduct)
 //router.delete('/:IdCategory',productController.deleteProductId)
 
 module.exports=router;
