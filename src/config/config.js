f92b594a767a9d377fcf7cf65bc10e97f124739d
Fernando Andres Lopez <fernando.lopez1@ibm.com>
2020-07-18 19:42:09 -0300

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,12 @@
 
 process.env.DB_MONGO = 'mongodb://pr-ecommerce-user:root@e-commerce-shard-00-00-s4yxw.mongodb.net:27017,e-commerce-shard-00-01-s4yxw.mongodb.net:27017,e-commerce-shard-00-02-s4yxw.mongodb.net:27017/test?ssl=true&replicaSet=e-commerce-shard-0&authSource=admin&retryWrites=true&w=majority'
 process.env.SECRET = 'fernandolopez'
+process.env.EMAIL='proyectosreact@gmail.com'
+process.env.PASS='academiatemple'
+process.env.HOST='localhost:4000'
+process.env.CLOUD_NAME='dzfcc45y1'
+process.env.API_KEY='919487441881635'
+process.env.API_SECRET='YhHB8XW5gPCb99FhL4gNzCf7rjs'
 
 const CodeReques = {
   OK: 0,
